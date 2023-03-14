d924fb91fca3b40df7933f5eea8bf34a1c0b5621
hoangnguyen <minhhoange10cn@gmail.com>
2023-03-14 15:22:45 +0700

    add docker

--- src/test/java/com/example/catalogservice/domain/BookServiceTest.java
+++ src/test/java/com/example/catalogservice/domain/BookServiceTest.java
@@ -24,7 +24,7 @@ class BookServiceTest {
     @Test
     void whenBookToCreateAlreadyExistsThenThrows() {
         var bookIsbn = "1234561232";
-        var bookToCreate = new Book(bookIsbn, "Title", "Author", 9.90);
+        var bookToCreate = Book.of(bookIsbn, "Title", "Author", 9.90, "Polarsophia");
         when(bookRepository.existsByIsbn(bookIsbn)).thenReturn(true);
         assertThatThrownBy(() -> bookService.addBookToCatalog(bookToCreate))
                 .isInstanceOf(BookAlreadyExistsException.class)
